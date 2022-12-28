3ddb80355db6e5b9900546a4895a2a8a998b80ab
Carlos E Alford M <10241920+CarlosEAM@users.noreply.github.com>
2022-12-28 00:27:46 +0000

    implement header checker

--- mk2html.py
+++ mk2html.py
@@ -1,4 +1,5 @@
 from os.path import exists
+import re
 
 
 # get the markdown file from /docs folder
@@ -19,16 +20,39 @@ def get_md_file(title):
   # give file back as string
   return file_info
 
+# @description check for header in string
+# @params file_line {string}
+# @returns {tuple} of two values {bool, string}
+def get_header(file_line):
+  header = re.match(r'#{1,6}\s', file_line)
+  if header:
+    header_level = header.end() - 1
+    html_header = f'<h{header_level}>{file_line[header.end():]}</h{header_level}>'
+    return (True, html_header)
+  else:
+    return (False, 'Header not found')
 
 def init_parser():
-  file_name = 'HTML'
   print('\n## Welcome this is Markdown to HTML parser ##\n')
-  print(f'.. Lets get the file: {file_name}')
-  f = get_md_file(file_name)
+  # global list store the created html tags
+  html_list = []
 
-  print('.. Here is what the file says:')
-  print(f'.. {f}')
+  # lets get the file
+  file_name = 'HTML'
+  md_file = get_md_file(file_name)
+  # split file into lines
+  md_file_lines = re.split(r'[\n\r]+', md_file)
 
-  print('')
+  while len(md_file_lines) > 0:
+    # lets check for header
+    header = get_header(md_file_lines[0])
+    if header[0]:
+      html_list.append(header[1])
+      md_file_lines.pop(0)
+      continue
+
+    # TODO: temp line. will remove an item from the list on each iteration
+    # stopping an infinite loop
+    md_file_lines.pop(0)
 
 init_parser()
